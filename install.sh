af0ced5cfe282267a2af40bbef6415a6f6442523
Sabin Marcu <sabinmarcu@gmail.com>
2016-07-16 10:40:36 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,19 @@
 #!/bin/bash
 
 cd $HOME
-git clone http://github.com/sabinmarcu/vimfiles.git
+
+if [ $(command -v git) ]; then
+  git clone http://github.com/sabinmarcu/vimfiles.git
+else
+  if [ $(command -v curl) ]; then
+    curl -O -L https://github.com/sabinmarcu/vimfiles/archive/master.zip
+  else
+    wget https://github.com/sabinmarcu/vimfiles/archive/master.zip
+  fi
+  unzip master.zip
+  mv vimfiles-master vimfiles
+  rm master.zip
+fi
+
 cd vimfiles
 ./configure
