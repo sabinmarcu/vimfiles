cd1609e91027264d543a5ac497dfe64d2cd0eacb
Sabin Marcu <sabinmarcu@gmail.com>
2017-01-13 11:41:46 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,4 +17,11 @@ else
 fi
 
 cd .vimfiles
-sh ./configure
+PWD=$(pwd)
+
+mkdir $PWD/.vim
+ls -a | grep vim | xargs -I % ln -s $PWD/% $HOME/%
+
+git clone http://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+
+vim +BundleInstall +BundleClean +qall
